6e0065bd234558e374a58cb9e4a854931cfc749a
NaveenRobotics <naveenrobotics@live.in>
2016-10-02 14:11:31 +0530

    Update simple mail.ps1

--- email/simple mail.ps1
+++ email/simple mail.ps1
@@ -1,10 +1,9 @@
-$EmailFrom = "keylogger <roboticslabkeylogger@gmail.com>"
-$EmailTo = "sakthinaveen251993@gmail.com" 
+$EmailFrom = "Mailer <from@mail.com>"
+$EmailTo = "to@mail.com" 
 $Subject = "hello, world!" 
 $Body = "Test mail from powershell" 
 $SMTPServer = "smtp.gmail.com" 
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
 $SMTPClient.EnableSsl = $true 
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("roboticslabkeylogger", "roboticslabkeylogger123"); 
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("username", "password"); 
 $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
-
